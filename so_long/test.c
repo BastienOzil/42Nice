20840cc80558ef11639509caa3431e7cd5360888
Bozil <161837532+BastienOzil@users.noreply.github.com>
2025-01-21 16:56:13 +0100

    Add files via upload

--- so_long/test.c
+++ so_long/test.c
@@ -61,7 +61,8 @@ int	main(void)
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 								&img.endian);
 }*/
-#include "includes/test.h"
+
+/*#include "includes/test.h"
 
 typedef struct	s_data {
 	void	*img;
@@ -91,7 +92,7 @@ int	main(void)
 	mlx_win = mlx_new_window(mlx, 1000, 600, "So_long");
 	img.img = mlx_new_image(mlx, 1000, 600);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-								&img.endian);
+								&img.endian);*/
 	/*my_mlx_pixel_put(&img, 5, 5, 0x00FFFFFF);
 	my_mlx_pixel_put(&img, 5, 6, 0x00FFFFFF);
 	my_mlx_pixel_put(&img, 5, 7, 0x00FFFFFF);
@@ -168,6 +169,39 @@ int	main(void)
     x++;
     }
 	*/
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	/*mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
+}*/
+
+#include "includes/test.h"
+
+#define ESC = 53
+
+typedef struct s_vars{
+    void    *mlx;
+    void    *win;
+}t_vars;
+
+int quit(int keycode, t_vars *vars)
+    {
+        if (keycode == ESC)
+        {
+            mlx_destroy_window(vars->mlx, vars->win);
+            free(vars);
+            exit();
+        }
+    }
+
+int main()
+{
+    t_vars  *vars;
+
+    vars = NULL;
+    vars = malloc(sizeof(t_vars));
+
+    vars->mlx = mlx_init();
+    vars->win = mlx_new_window(vars->mlx, 1920 , 1080 , "HELLO");
+
+    mlx_hook(vars->win, 2, 0, quit, vars);
+    mlx_loop(vars->mlx);
 }
