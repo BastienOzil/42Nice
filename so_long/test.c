070e7a99f1730d40b09bfff2719d7f51282efcf8
Bozil <161837532+BastienOzil@users.noreply.github.com>
2025-01-27 10:59:16 +0100

    Add files via upload

--- so_long/test.c
+++ so_long/test.c
@@ -174,23 +174,63 @@ int	main(void)
 }*/
 
 #include "includes/test.h"
+#include <stdlib.h>
+#include <stdio.h>
 
-#define ESC = 53
+#define ESC     65307
+#define W       119
+#define A	    97
+#define S	    115
+#define D	    100
 
 typedef struct s_vars{
     void    *mlx;
     void    *win;
+    int     box_x;
+    int     box_y;
 }t_vars;
 
-int quit(int keycode, t_vars *vars)
+int render_square(t_vars *vars)
+{
+    int x;
+    int y;
+
+    x = 350;
+    y = 350;
+    while(x < 450)
     {
+        while(y < 450)
+        {
+        mlx_pixel_put(vars->mlx, vars->win, x + vars->box_x, y + vars->box_y, 0xFFDDFF);
+        y++;
+        }
+    y = 350;
+    x++;
+    }
+    return(1);
+}
+int keys(int keycode, t_vars *vars)    
+	{
         if (keycode == ESC)
         {
             mlx_destroy_window(vars->mlx, vars->win);
             free(vars);
-            exit();
+            exit(0);
         }
-    }
+        else if (keycode == W)
+            vars->box_y -= 10;
+        else if (keycode == A)
+            vars->box_x -= 10;
+        else if (keycode == S)
+            vars->box_y += 10;
+        else if (keycode == D)
+            vars->box_x += 10;
+		else
+			printf("Key code is : %d\n", keycode);
+		// render_square(vars);
+    	return(0/*render_square(vars)*/);
+	}
+
 
 int main()
 {
@@ -199,9 +239,15 @@ int main()
     vars = NULL;
     vars = malloc(sizeof(t_vars));
 
-    vars->mlx = mlx_init();
-    vars->win = mlx_new_window(vars->mlx, 1920 , 1080 , "HELLO");
+    vars->box_x = 0;
+    vars->box_y = 0;
 
-    mlx_hook(vars->win, 2, 0, quit, vars);
+    vars->mlx = mlx_init();
+    vars->win = mlx_new_window(vars->mlx, 1920 , 1080 , "So_long");
+
+    mlx_loop_hook(vars->mlx, render_square, vars);
+	mlx_key_hook(vars->win, keys, &vars);
+    //mlx_hook(vars->win, 2, 0, keys, vars);
     mlx_loop(vars->mlx);
+	return (0);
 }
